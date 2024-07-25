5b6e4dbc66b8a95640d5e54a38837aa7b9165ab2
Kasun Kavinda <kkhettiarachchi10@gmail.com>
2024-07-26 01:40:25 +0530

    refactor the item card

--- src/components/StoreDetails.tsx
+++ src/components/StoreDetails.tsx
@@ -1,4 +1,20 @@
 import { useState, useEffect } from 'react';
+import riyasewana from "../assets/img/companies/riyasewana.png";
+import abans from "../assets/img/companies/abans.png";
+import autolanka from "../assets/img/companies/autolanka.jpg";
+import celltronic from "../assets/img/companies/celltronics.jpeg";
+import dialcom from "../assets/img/companies/dialcom.png";
+import laptop from "../assets/img/companies/laptop.lk.png";
+import life_mobile from "../assets/img/companies/lifemobile.jpg";
+import nanotec from "../assets/img/companies/nanotec.jpeg";
+import otc from "../assets/img/companies/otc.png";
+import redtech from "../assets/img/companies/redtech.jpeg";
+import saleme from "../assets/img/companies/saleme.png";
+import xmobile from "../assets/img/companies/xmobile.jpeg";
+import {Button, Slider} from "@nextui-org/react";
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 
 interface Product {
     _id: string;
@@ -7,11 +23,35 @@ interface Product {
     description: string;
     price: string;
     site: string;
+    url: string;
 }
+
+const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+};
 
 interface StoreDetailsProps {
     category: string;
 }
+
+const siteLogos: { [key: string]: string } = {
+    riyasewana: riyasewana,
+    abans: abans,
+    autolanka: autolanka,
+    celltronic: celltronic,
+    dialcom: dialcom,
+    laptop: laptop,
+    life_mobile: life_mobile,
+    nanotec: nanotec,
+    otc: otc,
+    redtech: redtech,
+    saleme: saleme,
+    xmobile: xmobile,
+};
 
 const PAGE_SIZE = 3;
 
@@ -21,6 +61,7 @@ function StoreDetails({ category }: StoreDetailsProps) {
     const [error, setError] = useState<string | null>(null);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
+    const [expandedProductId, setExpandedProductId] = useState<string | null>(null);
 
     useEffect(() => {
         const fetchProducts = async (page: number, category: string) => {
@@ -35,20 +76,18 @@ function StoreDetails({ category }: StoreDetailsProps) {
                 const data = await response.json();
                 if (category === 'laptops') {
                     setProducts(data.laptops);
-                }else if (category === 'phones') {
+                } else if (category === 'phones') {
                     setProducts(data.phones);
-                }else if (category === 'bikes') {
+                } else if (category === 'bikes') {
                     setProducts(data.bike);
-                }else if (category === 'cars') {
+                } else if (category === 'cars') {
                     setProducts(data.car);
-                }else if (category === 'microphones') {
+                } else if (category === 'microphones') {
                     setProducts(data.microphones);
                 }
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
-
-
                 setTotalPages(data.totalPages || 1);
             } catch (e: any) {
                 setError(e.message);
@@ -64,7 +103,11 @@ function StoreDetails({ category }: StoreDetailsProps) {
     if (error) return <p>Error: {error}</p>;
 
     const handleMoreInfoClick = (productId: string) => {
-        console.log(`More info clicked for product ${productId}`);
+        if (expandedProductId === productId) {
+            setExpandedProductId(null); // Collapse if already expanded
+        } else {
+            setExpandedProductId(productId); // Expand the clicked card
+        }
     };
 
     const handlePageChange = (newPage: number) => {
@@ -76,36 +119,106 @@ function StoreDetails({ category }: StoreDetailsProps) {
 
     return (
         <div className="container mx-auto px-4">
-            <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-                {products.map(product => (
-                    <div key={product._id} className="relative max-w-xs rounded-lg overflow-hidden shadow-lg bg-white transition-transform transform hover:scale-105" onClick={() =>handleMoreInfoClick(product._id)}>
-                        <div className="h-64 overflow-hidden">
-                            <img
-                                className="w-full h-full object-cover transition-transform duration-300 ease-in-out transform group-hover:scale-110"
-                                src={product.image[0]}
-                                alt={`Product ${product._id}`}
-                            />
-                        </div>
-                        <div className="px-6 py-4">
-                            <div className="font-semibold text-lg mb-2">{product.title}</div>
-                            <p className="text-gray-800 text-sm mb-2">Rs: {product.price}</p>
-                            <div className="font-medium text-sm mb-4 text-gray-600">From: {product.site}</div>
-                            <button
-                                className="bg-gradient-to-r from-purple-500 to-indigo-500 text-white py-2 px-4 rounded hover:from-purple-600 hover:to-indigo-600 transition-colors duration-300 w-full z-10 relative"
+            <div className="relative">
+                {/* Render the expanded card at the top */}
+                {expandedProductId && (
+                    products
+                        .filter(product => product._id === expandedProductId)
+                        .map(product => (
+                            <div
+                                key={product._id}
+                                className="fixed top-[7vh] left-0 right-0 z-20 bg-white shadow-lg h-[60vh] w-[80vw] mx-auto rounded-lg p-4"
+                                style={{ maxHeight: 'calc(100vh - 2rem)', overflowY: 'auto' }}
+                            >
+                                {product.image.length > 1 ? (
+                                    <Slider {...sliderSettings}>
+                                        {product.image.map((img, index) => (
+                                            <div key={index}>
+                                                <img
+                                                    className="w-[25vw] h-[55vh] object-cover absolute left-1 top-2 bottom-2"
+                                                    src={img}
+                                                    alt={`Product ${product._id} - ${index}`}
+                                                />
+                                            </div>
+                                        ))}
+                                    </Slider>
+                                ) : (
+                                    <img
+                                        className="w-[25vw] h-[55vh] object-cover absolute left-1 top-2 bottom-2"
+                                        src={product.image[0]}
+                                        alt={`Product ${product._id}`}
+                                    />
+                                )}
+                                <div className="relative">
+                                    {/*<img*/}
+                                    {/*    className=""*/}
+                                    {/*    src={product.image[0]}*/}
+                                    {/*    alt={`Product ${product._id}`}*/}
+                                    {/*/>*/}
+                                    <button
+                                        className="absolute top-4 right-4 text-3xl text-black"
+                                        onClick={() => setExpandedProductId(null)}
+                                    >
+                                        &times;
+                                    </button>
+                                    <div className="p-6 absolute right-20  top-0">
+
+                                        <h2 className="text-xl font-semibold absolute right-20">{product.title}</h2>
+                                        <p className="text-lg font-bold text-blue-400 absolute right-20 mt-14">Rs: {product.price}</p>
+                                        <p className="text-sm text-gray-600 absolute right-20 mt-24">From: {product.site}</p>
+                                        <p className="mt-40 w-[35vw] ">{product.description}</p>
+
+                                        <a href={product.url}>
+                                            <Button className=" mt-56 absolute right-20 bg-blue-500 font-bold hover:shadow-2xl rounded" >
+                                                Buy Now
+                                            </Button>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                )}
+
+                {/* Render the other products below the expanded card */}
+                <div
+                    className={`mt-8 ${expandedProductId ? 'pt-[50vh]' : ''} grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6`}>
+                    {products
+                        .filter(product => product._id !== expandedProductId)
+                        .map(product => (
+                            <div
+                                key={product._id}
+                                className="relative max-w-xs rounded-lg overflow-hidden shadow-lg bg-white transition-transform transform hover:scale-105 cursor-pointer"
                                 onClick={() => handleMoreInfoClick(product._id)}
                             >
-                                More Information
-                            </button>
-                        </div>
-                        <div
-                            className="absolute inset-0 bg-gradient-to-t from-black via-transparent to-transparent opacity-0 hover:opacity-80 transition-opacity duration-300 flex items-center justify-center p-4"
-                        >
-                            <p className="text-white text-center text-xs md:text-sm">
-                                {product.description}
-                            </p>
-                        </div>
-                    </div>
-                ))}
+                                <div className="h-64 overflow-hidden relative">
+                                    <img
+                                        className="w-full h-full object-cover transition-transform duration-300 ease-in-out"
+                                        src={product.image[0]}
+                                        alt={`Product ${product._id}`}
+                                    />
+                                    <img
+                                        className="absolute top-1 right-1 w-20 h-20"
+                                        src={siteLogos[product.site] || 'path/to/default/logo.png'}
+                                        alt={product.site}
+                                    />
+                                </div>
+                                <div className="px-6 py-6">
+                                    <div className="font-semibold text-lg mb-2">{product.title}</div>
+                                    <p className="font-bold mb-2 text-blue-400">Rs: {product.price}</p>
+                                    <div className="font-medium text-sm mb-8 text-gray-600">From: {product.site}</div>
+                                    <button
+                                        className="bg-gradient-to-r from-purple-500 to-indigo-500 text-white py-2 px-4 rounded hover:from-purple-600 hover:to-indigo-600 transition-colors duration-300 w-full"
+                                        onClick={(e) => {
+                                            e.stopPropagation(); // Prevent click event from bubbling up
+                                            handleMoreInfoClick(product._id);
+                                        }}
+                                    >
+                                        More Information
+                                    </button>
+                                </div>
+                            </div>
+                        ))}
+                </div>
             </div>
             <div className="flex justify-center mt-8 space-x-2">
                 <button
