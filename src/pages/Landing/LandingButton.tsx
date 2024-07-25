452d1a79dc842fb38c5c24522edb97adddd04764
Ujitha Migara <ujithamigara@gmail.com>
2024-07-25 19:16:13 +0530

    add

--- src/pages/Landing/LandingButton.tsx
+++ src/pages/Landing/LandingButton.tsx
@@ -1,13 +1,13 @@
-import React from "react";
 
-const LandingButton = ({ text, bgColor, textColor, handler = () => {} }) => {
+
+const LandingButton = ({ text, bgColor, textColor }) => {
   return (
-    <button
-      className={`${bgColor} ${textColor} cursor-pointer hover:scale-105
+      <button
+          className={`${bgColor} ${textColor} cursor-pointer hover:scale-105
     duration-300 py-2 px-8 rounded-full relative z-10 font-bold text-lg`}
-    >
-      {text}
-    </button>
+      >
+        {text}
+      </button>
   );
 };
 
