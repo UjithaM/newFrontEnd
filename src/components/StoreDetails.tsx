143076930956e2b12c7085c88a8dfa8857cd0ad4
Ujitha Migara <ujithamigara@gmail.com>
2024-07-26 08:30:56 +0530

    bug ix

--- src/components/StoreDetails.tsx
+++ src/components/StoreDetails.tsx
@@ -27,7 +27,7 @@ function StoreDetails({ category }: StoreDetailsProps) {
             setLoading(true);
             setError(null);
             try {
-                const response = await fetch(`https://offdealz.lk/api/?page=${page}`, {
+                const response = await fetch(`https://offdealz.lk/api/${category}?page=${page}`, {
                     headers: {
                         'X-API-KEY': import.meta.env.VITE_API_KEY,
                     },
